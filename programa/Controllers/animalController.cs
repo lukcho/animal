ddafc50cde636496df2cddc02ba909b670bd1546
Lukcho <luiscorrea1988@hotmail.com>
2014-07-02 15:19:01 -0500

    Initial commit

---
+++ programa/Controllers/animalController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using programa.Models;
+
+namespace programa.Controllers
+{
+    public class animalController : Controller
+    {
+        private programaContext db = new programaContext();
+
+        //
+        // GET: /animal/
+
+        public ActionResult Index()
+        {
+            return View(db.animals.ToList());
+        }
+
+        //
+        // GET: /animal/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            animal animal = db.animals.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animal);
+        }
+
+        //
+        // GET: /animal/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /animal/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.animals.Add(animal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(animal);
+        }
+
+        //
+        // GET: /animal/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            animal animal = db.animals.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animal);
+        }
+
+        //
+        // POST: /animal/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(animal animal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(animal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(animal);
+        }
+
+        //
+        // GET: /animal/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            animal animal = db.animals.Find(id);
+            if (animal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(animal);
+        }
+
+        //
+        // POST: /animal/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            animal animal = db.animals.Find(id);
+            db.animals.Remove(animal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ programa/Models/animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace programa.Models
+{
+    public class animal
+    {
+        public int animalID { set; get; }
+        public string nombre { set; get; }
+        public int edad { set; get; }
+    }
+}
